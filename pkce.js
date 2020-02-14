acae1409bd4884aef5825c131b113b598ea1717b
Adrian Lazar <adrian.lazar95@outlook.com>
2020-02-14 18:16:25 +0200

    Update pkce.js

--- pkce.js
+++ pkce.js
@@ -1,6 +1,6 @@
-let generateCodeVerifier = () => 
+var generateCodeVerifier = () => 
 {
-    let code_verifier = window.localStorage.getItem("code_verifier");
+    var code_verifier = window.localStorage.getItem("code_verifier");
     if (!code_verifier)
     {
         code_verifier = generateRandomString(128);
@@ -12,9 +12,9 @@ let generateCodeVerifier = () =>
 }
 
 
-let generateCodeChallenge = (code_verifier) => 
+var generateCodeChallenge = (code_verifier) => 
 {
-    let code_challenge = window.localStorage.getItem("code_challenge");
+    var code_challenge = window.localStorage.getItem("code_challenge");
     if (!code_challenge)
     {
         code_challenge = base64URL(CryptoJS.SHA256(code_verifier));
@@ -23,20 +23,20 @@ let generateCodeChallenge = (code_verifier) =>
     return code_challenge;
 }
 
-let generateRandomString = (length) => 
+var generateRandomString = (length) => 
 {
-    let text = window.localStorage.getItem("random_string");
+    var text = window.localStorage.getItem("random_string");
     if(!text){
         text = "";
-        let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-        for (let i = 0; i < length; i++) {
+        var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        for (var i = 0; i < length; i++) {
           text += possible.charAt(Math.floor(Math.random() * possible.length));
         }
     }
     return text;
 }
 
-let base64URL = (string) =>
+var base64URL = (string) =>
 {
     return string.toString(CryptoJS.enc.Base64).replace(/=/g, '').replace(/\+/g, '-').replace(/\//g, '_')
 }
